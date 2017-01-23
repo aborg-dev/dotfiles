78c0ee2630b64c7f20cdc01370b4360b1233e67a
akashin <kashin.andrej@gmail.com>
2017-01-23 11:41:14 +0300

    Add temporary file with setup instructions

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s /Users/acid/Repos/Wiki/content /Users/acid/notes
+
+fix .bin (ipython, ipython3)
+ln -s Cloud/Yandex.Disk.localized/Code code
+ln -s Cloud/Yandex.Disk.localized/Books books
+ln -s Cloud/Yandex.Disk.localized/Documents docs
