c5ad63e42cc44ba03289f928abf3c6b864158df6
akashin <kashin.andrej@gmail.com>
2021-04-21 21:05:31 +0100

    [vim] Add shortcuts to jump between windows

--- vim/init.lua
+++ vim/init.lua
@@ -4,6 +4,9 @@ require('plugins')
 -- TODO: Shortcut to jump another split with Ctrl-l.
 -- TODO: Shortcut to open vim config.
 -- TODO: Replace shouldn't replace yank buffer.
+-- Allow pasting same thing many times
+-- {'p', '""p:let @"=@0<CR>'},
+
 
 -- Editing settings.
 local indent = 2
@@ -21,14 +24,21 @@ vim.o.termguicolors = true
 vim.g.mapleader = " "
 vim.g.localmapleader = ","
 -- Exit insert mode without going all the way to escape key!
-vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true})
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
+
+-- Jump between windows with less typing.
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
 local wk = require('whichkey_setup')
 
 local keymap = {
   -- 'show_all_buffers = true' is needed to also show hidden buffers.
   [","] = {':lua require("telescope.builtin").buffers({show_all_buffers = true})<CR>', 'buffers'},
-  [" "] = {':lua require("telescope.builtin").find_files()<CR>', 'find_files'},
+  -- TODO: Why wouldn't it work after some delay???
+  [" "] = {':lua require("telescope.builtin").find_files({previewer = false})<CR>', 'find_files'},
   -- [" "] = {':lua require("telescope.builtin").find_files(require("telescope.themes").get_dropdown({ winblend = 10 }))<CR>', 'find_files'},
 }
 
