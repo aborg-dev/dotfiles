159e406605a150e514b6a5531c0b402579c7d671
akashin <kashin.andrej@gmail.com>
2021-04-22 23:41:03 +0100

    [vim] Add shortcuts for global clipboard management

--- vim/lua/keymap.lua
+++ vim/lua/keymap.lua
@@ -1,9 +1,6 @@
 -- Global key mappings.
 
 -- TODO: Shortcut to open vim config.
--- TODO: Replace shouldn't replace yank buffer.
--- TODO: Allow pasting same thing many times
--- {'p', '""p:let @"=@0<CR>'},
 
 -- These keys serve as a prefix in many commands.
 vim.g.mapleader = " "
@@ -17,6 +14,17 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Bindings to copy and paste to a named register. 
+-- When you yank without any modifier, text is placed into default register.
+-- When you delete some text, it is also automatically placed into the default
+-- register. This creates an unfortunate situation, when you can't paste with
+-- replacement the same yanked text multiple times. By using a named register 
+-- we avoid this problem.
+-- We use a special '+' register here that also corresponds to OS default clipboard,
+-- so the text yanked this way can be pasted in other applicatins.
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', {})
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', {})
 
 -- Most of the plugins I use expose their shortcuts in a structured hierarchical
 -- manner using which-key plugin. The configuration is incrementally extended in
