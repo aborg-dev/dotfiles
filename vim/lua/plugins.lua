f8c82cc3ca5271563c5711b7413b1ed140270b1b
akashin <kashin.andrej@gmail.com>
2021-05-02 23:25:28 +0100

    [vim] Fix warnings in pluggins.lua

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,7 +1,7 @@
 -- Only required if you have packer configured as `opt`.
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
+return require('packer').startup(function(use)
   -- Packer can manage itself
   use { 'wbthomason/packer.nvim', opt = true }
 
