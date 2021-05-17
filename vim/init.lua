7da4d83a6ca8c201ff1ba9d7365a404966d26a5b
akashin <kashin.andrej@gmail.com>
2021-05-17 21:22:26 +0100

    [vim] Add bootstrap code to install plugin on first launch

--- vim/init.lua
+++ vim/init.lua
@@ -9,19 +9,25 @@ local pack_path = fn.stdpath("data") .. "/site/pack"
 function ensure (user, repo)
   -- Ensures a given github.com/USER/REPO is cloned in the pack/packer/start directory.
   local install_path = fmt("%s/packer/opt/%s", pack_path, repo, repo)
+  local installed = false
   if fn.empty(fn.glob(install_path)) > 0 then
     execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+    installed = true
   end
-	execute(fmt("packadd %s", repo))
+  execute(fmt("packadd %s", repo))
+  return installed
 end
 
 -- Packer is our plugin manager.
-ensure("wbthomason", "packer.nvim")
+if ensure("wbthomason", "packer.nvim") then
+  require('plugins')
+  execute('PackerInstall')
+end
 
 -- This must be executed before loading aniseed to use the correct configuration.
 vim.g['aniseed#env'] = {
-	output = vim.fn.stdpath('config') .. '/lua/.compiled-lua',
-	module = 'startup'
+  output = vim.fn.stdpath('config') .. '/lua/.compiled-lua',
+  module = 'startup'
 }
 
 -- Aniseed compiles our Fennel code to Lua and loads it automatically.
