e8d6c2956a6b8e3f9919b744c927593aa4d619f1
Andrei Kashin <kashin.andrej@gmail.com>
2022-09-02 11:54:58 +0100

    Update setup scripts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 ln -s /Users/acid/Repos/Wiki/content /Users/acid/notes
-sudo apt-get install zsh
-
-fix .bin (ipython, ipython3)
 ln -s Cloud/Yandex.Disk.localized/Code code
 ln -s Cloud/Yandex.Disk.localized/Books books
 ln -s Cloud/Yandex.Disk.localized/Documents docs
