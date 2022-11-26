aded2bbd2ea018c3742718898d5bf722eae54bfd
Andrei Kashin <kashin.andrej@gmail.com>
2022-11-26 08:50:53 +0000

    [vim] Add trouble.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -200,4 +200,16 @@ return require("packer").startup(function(use)
       require "plugins.toggleterm_config"
     end,
   }
+
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+  }
 end)
