896220cfc0820499e7332aa3a542f18a49b69b61
Andrei Kashin <kashin.andrej@gmail.com>
2023-08-25 20:27:30 +0200

    Remove unused setup script

--- setup.sh
+++
@@ -1,4 +0,0 @@
-ln -s /Users/acid/Repos/Wiki/content /Users/acid/notes
-ln -s Cloud/Yandex.Disk.localized/Code code
-ln -s Cloud/Yandex.Disk.localized/Books books
-ln -s Cloud/Yandex.Disk.localized/Documents docs
