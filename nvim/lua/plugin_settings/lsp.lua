1efa626b3b4f54775d9bc453631bb653667b6c2a
Andrei Kashin <kashin.andrej@gmail.com>
2023-12-18 14:57:59 +0100

    [vim] Enable fennel LSP

--- nvim/lua/plugin_settings/lsp.lua
+++ nvim/lua/plugin_settings/lsp.lua
@@ -82,14 +82,29 @@ lsp_config.beancount.setup {
   },
 }
 
-lsp_config.pyright.setup {}
-
-lsp_config.clangd.setup {}
-
 vim.api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
   { pattern = { "*.bean" }, command = "setlocal foldmethod=marker" }
 )
+
+lsp_config.pyright.setup {}
+
+lsp_config.clangd.setup {}
+
+lsp_config.fennel_language_server.setup {
+	settings = {
+		fennel = {
+			workspace = {
+				-- If you are using hotpot.nvim or aniseed,
+				-- make the server aware of neovim runtime files.
+				library = vim.api.nvim_list_runtime_paths(),
+			},
+			diagnostics = {
+				globals = {'vim'},
+			},
+		},
+	},
+}
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "LspAttached",
