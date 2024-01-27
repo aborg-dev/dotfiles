bd232e0a79d37170b41cd415d8d9c9074b37a991
Andrei Kashin <kashin.andrej@gmail.com>
2024-01-27 19:28:10 +0000

    [vim] Add noice plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -212,6 +212,22 @@ return require("lazy").setup({
       require("nvim-autopairs").setup {}
     end,
   },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
 }, {
   dev = {
     path = "~/.local/share/nvim/nix",
