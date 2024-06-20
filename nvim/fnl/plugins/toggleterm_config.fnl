c223f7aba9f971af5063b8651f23ed4c9e8c3c10
Andrei Kashin <kashin.andrej@gmail.com>
2024-06-20 16:56:54 +0100

    [vim] Fix toggleterm shell

--- nvim/fnl/plugins/toggleterm_config.fnl
+++ nvim/fnl/plugins/toggleterm_config.fnl
@@ -3,6 +3,7 @@
 (tt.setup {
   :open_mapping :<C-t>
   :terminal_mappings true ;; Apply mappings in the opened terminals.
+  :shell "/home/akashin/.nix-profile/bin/fish"
   ;; :insert_mappings false ;; I don't need mappings in insert mode.
   :direction "float"
 	:float_opts {
--- nvim/lua/plugins/toggleterm_config.lua
+++ nvim/lua/plugins/toggleterm_config.lua
@@ -1,3 +1,3 @@
 -- [nfnl] Compiled from fnl/plugins/toggleterm_config.fnl by https://github.com/Olical/nfnl, do not edit.
 local tt = require("toggleterm")
-return tt.setup({open_mapping = "<C-t>", terminal_mappings = true, direction = "float", float_opts = {border = "curved"}})
+return tt.setup({open_mapping = "<C-t>", terminal_mappings = true, shell = "/home/akashin/.nix-profile/bin/fish", direction = "float", float_opts = {border = "curved"}})
