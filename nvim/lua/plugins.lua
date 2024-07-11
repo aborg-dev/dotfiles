1d9bf5549a5934a9da684b29b6e9739499072670
Andrei Kashin <kashin.andrej@gmail.com>
2024-07-11 10:53:49 +0100

    [nvim] Switch to flash.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -71,12 +71,19 @@ return require("lazy").setup({
   -- Add shortcuts to jump between vim and tmux splits.
   "christoomey/vim-tmux-navigator",
 
-  -- Quickly jump within the buffer with s{a}{b} and S{a}{b}.
+  -- Quickly jump to a place in visible buffers with s{a}{b}.
   {
-    "justinmk/vim-sneak",
-    config = function()
-      vim.g["sneak#label"] = 1
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
   },
 
   -- Auto-completion plugin.
