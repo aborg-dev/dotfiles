ce442af2656fe5cf5c2851c612b7bcb75b7cd035
Andrei Kashin <kashin.andrej@gmail.com>
2024-07-19 08:56:43 +0100

    [nvim] Add surround and spectre

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -226,6 +226,24 @@ return require("lazy").setup({
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
+  },
+
+  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
