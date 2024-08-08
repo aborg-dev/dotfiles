56b7510580b57d1fb497d610ee41292a5a5bb479
Andrei Kashin <kashin.andrej@gmail.com>
2024-08-08 15:22:06 +0100

    Run formatter on flake.nix

--- flake.nix
+++ flake.nix
@@ -13,46 +13,36 @@
   };
 
   outputs = { nixpkgs, home-manager, catppuccin, ... }: {
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-      defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
 
-      homeConfigurations."akashin@glados" = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations."akashin@glados" =
+      home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./nix/glados.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
+        modules = [ ./nix/glados.nix catppuccin.homeManagerModules.catppuccin ];
       };
 
-      homeConfigurations."akashin@spire" = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations."akashin@spire" =
+      home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./nix/spire.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
+        modules = [ ./nix/spire.nix catppuccin.homeManagerModules.catppuccin ];
       };
 
-      homeConfigurations."akashin@tuxedo" = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations."akashin@tuxedo" =
+      home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./nix/tuxedo.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
+        modules = [ ./nix/tuxedo.nix catppuccin.homeManagerModules.catppuccin ];
       };
 
-      homeConfigurations."akashin" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        modules = [
-          ./nix/osx.nix
-        ];
-      };
-
-      homeConfigurations."akashin@odroid" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./nix/odroid.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
-      };
+    homeConfigurations."akashin" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [ ./nix/osx.nix ];
     };
+
+    homeConfigurations."akashin@odroid" =
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./nix/odroid.nix catppuccin.homeManagerModules.catppuccin ];
+      };
+  };
 }
