852bfc44340f80edaf2f82260899ace9e0e6e0d0
Andrei Kashin <kashin.andrej@gmail.com>
2024-08-16 11:40:21 +0100

    Migrate tuxedo to aborg

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
         modules = [ ./nix/spire.nix catppuccin.homeManagerModules.catppuccin ];
       };
 
-    homeConfigurations."akashin@tuxedo" =
+    homeConfigurations."aborg@tuxedo" =
       home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [ ./nix/tuxedo.nix catppuccin.homeManagerModules.catppuccin ];
