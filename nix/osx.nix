301e0223844b3044da77e2cca0e6ca6ff42320ef
Andrew Borg <git@aborg.dev>
2024-09-23 19:14:30 +0100

    [osx] Add yabai

--- nix/osx.nix
+++ nix/osx.nix
@@ -7,4 +7,7 @@
     username = "aborg";
     homeDirectory = "/Users/aborg";
   };
+
+  xdg.configFile."yabai/yabairc".source = ../yabai/yabairc;
+  xdg.configFile."skhd/skhdrc".source = ../yabai/skhdrc;
 }
