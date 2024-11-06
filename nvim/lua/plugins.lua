d3534f749067233c83c1cf88a25e5334ee99d5e2
Andrew Borg <git@aborg.dev>
2024-11-06 13:30:39 +0000

    [vim] Add Flash line jumping

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -87,6 +87,11 @@ return require("lazy").setup({
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      { "L",     mode = { "n", "x", "o" }, function() require("flash").jump({
+        search = { mode = "search", max_length = 0 },
+        label = { after = { 0, 0 } },
+        pattern = "^"
+      }) end,            desc = "Flash line" },
     },
   },
 
