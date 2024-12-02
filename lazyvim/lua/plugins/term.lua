504500cfeab00b9f315b7d75d0f0ac2da3dc2173
Andrew Borg <git@aborg.dev>
2024-12-02 16:41:42 +0000

    [lazyvim] Add toggleterm

---
+++ lazyvim/lua/plugins/term.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    open_mapping = "<C-t>",
+    -- Apply mappings in the opened terminals.
+    terminal_mappings = true,
+    shell = "~/.nix-profile/bin/fish",
+    -- I don't need mappings in insert mode.
+    insert_mappings = false,
+    direction = "float",
+    float_opts = {
+      border = "curved",
+    },
+  },
+}
