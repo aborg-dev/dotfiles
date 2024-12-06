39724b73916b49126a2fcf360ae721324cd9914b
Andrew Borg <git@aborg.dev>
2024-12-06 11:17:22 +0000

    [vim] Tweak defaults

--- lazyvim/lua/plugins/bullets.lua
+++ lazyvim/lua/plugins/bullets.lua
@@ -5,6 +5,6 @@ return {
   end,
   keys = {
     { "<localleader>t", "<cmd>ToggleCheckbox<cr>", desc = "Toggle checkbox" },
-    { "<C-CR>", "<cmd>InsertNewBullet<cr>", desc = "Insert new bullet" },
+    { "<C-CR>", "<cmd>InsertNewBullet<cr>", desc = "Insert new bullet", mode = { "n", "i" } },
   },
 }
