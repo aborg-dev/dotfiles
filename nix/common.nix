08eeac048132193d4145b78129f99f950f222254
Andrew Borg <git@aborg.dev>
2024-12-10 20:53:53 +0000

    [tmux] Simplify theme

--- nix/common.nix
+++ nix/common.nix
@@ -23,7 +23,7 @@
     ranger
     # Fonts that I'm using in my terminal.
     jetbrains-mono
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    nerd-fonts.jetbrains-mono
     zk
     atuin
     stylua
@@ -254,23 +254,12 @@
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
           set -g @catppuccin_flavour 'frappe'
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_right_separator " "
-          set -g @catppuccin_window_middle_separator " █"
-          set -g @catppuccin_window_number_position "right"
-
           set -g @catppuccin_window_default_fill "number"
           set -g @catppuccin_window_default_text "#W"
-
           set -g @catppuccin_window_current_fill "number"
           set -g @catppuccin_window_current_text "#W"
 
-          set -g @catppuccin_status_modules_right "directory session date_time"
-          set -g @catppuccin_status_left_separator  " "
-          set -g @catppuccin_status_right_separator ""
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-
+          set -g @catppuccin_status_modules_right "directory session"
           set -g @catppuccin_directory_text "#{pane_current_path}"
         '';
       }
