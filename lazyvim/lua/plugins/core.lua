96b5de35c581f0168260e7ff200df2459abbbe77
Andrew Borg <git@aborg.dev>
2024-12-11 21:49:57 +0000

    [vim] Update lazyvim

--- lazyvim/lua/plugins/core.lua
+++ lazyvim/lua/plugins/core.lua
@@ -15,25 +15,4 @@ return {
       inlay_hints = { enabled = false },
     },
   },
-  -- Disable the text completion.
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = vim.tbl_filter(function(source)
-        return not vim.tbl_contains({ "buffer", "nvim_lsp" }, source.name)
-      end, opts.sources)
-      table.insert(opts.sources, 1, {
-        name = "nvim_lsp",
-        entry_filter = function(entry, _)
-          return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-        end,
-      })
-    end,
-  },
-  {
-    "garymjr/nvim-snippets",
-    opts = {
-      friendly_snippets = false,
-    },
-  },
 }
