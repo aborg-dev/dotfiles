35f312ecf1aa25edfa64947b3559d665647de2a5
Andrew Borg <git@aborg.dev>
2025-01-20 18:45:04 +0000

    [nix] Add theseus

--- nix/theseus.nix
+++ nix/theseus.nix
@@ -1,10 +1,13 @@
 { ... }:
 
 {
-  imports = [ ./common.nix ];
+  imports = [ ./common.nix ./desktop.nix ];
 
   home = {
     username = "aborg";
     homeDirectory = "/home/aborg";
   };
+
+  xdg.configFile."alacritty/font.toml".source = ../alacritty/1080p_font.toml;
+  xdg.configFile."sway/font".source = ../sway/1080p_font;
 }
