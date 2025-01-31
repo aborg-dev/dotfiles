308d19d56c5871399921d08020feba04eb75d076
Andrew Borg <git@aborg.dev>
2025-01-31 09:06:15 +0000

    [vim] Enable super-tab

--- lazyvim/lua/plugins/blink.lua
+++ lazyvim/lua/plugins/blink.lua
@@ -4,6 +4,9 @@ return {
     enabled = function()
       return not vim.tbl_contains({ "markdown" }, vim.bo.filetype) and vim.bo.buftype ~= "prompt"
     end,
+    keymap = {
+      preset = "super-tab",
+    },
     sources = {
       default = { "codecompanion" },
       providers = {
